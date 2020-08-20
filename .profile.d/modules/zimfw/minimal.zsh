6e7f10a4503edae1b8f6524c6584c54bf402082f
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-08-20 13:33:25 +0200

    🎨 (Zim): Move ZimFw config to dedicated module

---
+++ .profile.d/modules/zimfw/minimal.zsh
@@ -0,0 +1,9 @@
+# Components on the left prompt
+export MNML_PROMPT=(mnml_ssh mnml_status mnml_keymap)
+
+# Components on the right prompt
+export MNML_RPROMPT=('mnml_cwd 2 0' mnml_git)
+
+# Components shown on info line
+export MNML_INFOLN=(mnml_err mnml_jobs mnml_uhp)
+export MNML_MAGICENTER=(mnml_me_dirs mnml_me_ls mnml_me_git)
