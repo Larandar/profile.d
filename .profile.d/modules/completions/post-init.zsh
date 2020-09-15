063259d4f5576d493432364d31fe4bc30e613fc6
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-09-15 10:05:11 +0200

    ⚡️ (Completion): Add completion for pipenv if installed

--- .profile.d/modules/completions/post-init.zsh
+++ .profile.d/modules/completions/post-init.zsh
@@ -1,4 +1,5 @@
 # Autocomplete for terraform and other bash completed CLI
 
 which terraform >/dev/null && complete -o nospace -C `which terraform` terraform
+which pipenv >/dev/null && eval "$(pipenv --completion)"
 
