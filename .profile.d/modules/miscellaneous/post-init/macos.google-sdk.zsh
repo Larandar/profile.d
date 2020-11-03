594d64954c291ccd75440dc270a559d9ec149259
Adrien 'Larandar' DUDOUIT-EXPOSITO <adrien@dudouit.me>
2020-11-03 09:28:46 +0100

    🐛 (GCP): Fix python version used for gcloud

--- .profile.d/modules/miscellaneous/post-init/macos.google-sdk.zsh
+++ .profile.d/modules/miscellaneous/post-init/macos.google-sdk.zsh
@@ -1,3 +1,6 @@
 # Google Cloud CLI
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
+
+# Google SDK does not yet support python 3.9: https://issuetracker.google.com/issues/170716950
+export CLOUDSDK_PYTHON=$(brew --prefix python@3.8)/bin/python3
